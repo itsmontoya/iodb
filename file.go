50a0e3000685a3be836833f85c8151d764fbcd15
Josh Montoya <josh@montoya.io>
2026-03-21 10:45:48 -0700

    minor clean up to file

--- file.go
+++ file.go
@@ -4,27 +4,32 @@ import (
 	"context"
 	"io"
 	"os"
-	"path"
+	"path/filepath"
 	"sync"
 
 	"github.com/itsmontoya/streambuf"
 )
 
-func newFile(dir string, e os.DirEntry) (out *File) {
+func newFile(dbPath, dir string, e os.DirEntry) (out *File) {
 	var f File
+	f.dbPath = dbPath
 	f.dir = dir
 	f.key = e.Name()
 	return &f
 }
 
+// File represents a single persisted value in the database.
 type File struct {
 	entry
 
-	// Transaction mutex, only used for writes
+	// Transaction mutex, used to guard stream initialization and writes
 	tmux sync.Mutex
-	s    *streambuf.Stream
+
+	s *streambuf.Stream
 }
 
+// Read opens a reader for the file and passes it to fn.
+// The reader is closed after fn returns.
 func (f *File) Read(fn func(io.Reader) error) (err error) {
 	var s *streambuf.Stream
 	if s, err = f.getStream(); err != nil {
@@ -39,9 +44,10 @@ func (f *File) Read(fn func(io.Reader) error) (err error) {
 	return fn(rc)
 }
 
+// Update writes file contents through fn and atomically replaces the file.
 func (f *File) Update(fn func(io.Writer) error) (err error) {
 	var tempFilepath string
-	if tempFilepath, err = tempFile(fn); err != nil {
+	if tempFilepath, err = tempFile(f.dbPath, fn); err != nil {
 		return err
 	}
 
@@ -52,10 +58,10 @@ func (f *File) Update(fn func(io.Writer) error) (err error) {
 	return nil
 }
 
-func (f *File) updateFromTemp(tempFilepath string) (err error) {
+func (f *File) updateFromTemp(tempPath string) (err error) {
 	f.tmux.Lock()
 	defer f.tmux.Unlock()
-	if err = os.Rename(tempFilepath, f.filepath()); err != nil {
+	if err = os.Rename(tempPath, f.filepath()); err != nil {
 		return err
 	}
 
@@ -74,14 +80,15 @@ func (f *File) getStream() (s *streambuf.Stream, err error) {
 		return f.s, nil
 	}
 
-	f.s, err = streambuf.NewStream(path.Join(f.dir, f.key))
-	return f.s, nil
+	f.s, err = streambuf.NewStream(filepath.Join(f.dir, f.key))
+	return f.s, err
 }
 
 func (f *File) filepath() (out string) {
-	return path.Join(f.dir, f.key)
+	return filepath.Join(f.dir, f.key)
 }
 
+// String returns the file key.
 func (f *File) String() string {
 	return f.key
 }
