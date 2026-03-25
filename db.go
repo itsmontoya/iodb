31506e6d02758ad0eb3c1ed629972b72396f7857
Josh Montoya <josh@montoya.io>
2026-03-24 20:08:36 -0700

    remove DB.String

--- db.go
+++ db.go
@@ -1,23 +1,14 @@
 package iodb
 
-import (
-	"fmt"
-)
-
 func New(dbPath string) (out *DB, err error) {
 	var db DB
-	if db.buckets, db.files, err = getBucketsAndFiles(dbPath); err != nil {
-		return nil, err
+	if db.Bucket, err = newBucket(dbPath, ""); err != nil {
+		return
 	}
 
-	fmt.Println("DB", &db)
 	return &db, nil
 }
 
 type DB struct {
-	Bucket
-}
-
-func (db *DB) String() string {
-	return fmt.Sprintf("Bucket<Buckets: %v Files: %v>", db.buckets, db.files)
+	*Bucket
 }
