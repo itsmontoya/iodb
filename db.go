bae383cdfdae7809a6e2c4fb268cf5f2d2cc981f
Josh Montoya <josh@montoya.io>
2026-03-28 14:12:15 -0700

    add GoDoc comments for DB

--- db.go
+++ db.go
@@ -2,6 +2,7 @@ package iodb
 
 import "os"
 
+// New creates or opens a database rooted at dbPath.
 func New(dbPath string) (out *DB, err error) {
 	if err = os.MkdirAll(dbPath, 0755); err != nil {
 		return nil, err
@@ -15,6 +16,7 @@ func New(dbPath string) (out *DB, err error) {
 	return &db, nil
 }
 
+// DB is the root container for buckets and files under a database path.
 type DB struct {
 	*Bucket
 }
