f12ffdce2716b9a4f0a503707bf1c1501c9df99a
Josh Montoya <josh@montoya.io>
2026-03-28 14:18:35 -0700

    move Cursor init to cursor.go

--- cursor.go
+++ cursor.go
@@ -2,7 +2,17 @@ package iodb
 
 import "github.com/itsmontoya/bst"
 
+func makeCursor(b bst.BST[*File]) (c Cursor) {
+	c.Cursor = b.Cursor()
+	return c
+}
+
 // Cursor iterates files in key order for a bucket snapshot.
 type Cursor struct {
 	*bst.Cursor[*File]
+}
+
+func (c *Cursor) cleanup() {
+	// Invalidate underlyhing bst.Cursor so it cannot be used outside of the mutex lock
+	c.Cursor = nil
 }
